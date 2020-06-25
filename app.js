8ba34717fed1568806aa8936f1d2a414e3ece792
Brian McPhillips <B.McPhillips89@gmail.com>
2020-06-24 17:55:02 -0700

    button is working colors are not

---
+++ app.js
@@ -0,0 +1,14 @@
+const nameButton = document.getElementById('submit-button');
+
+nameButton.addEventListener('click', () => {
+    const myInput = document.getElementById('text-input');
+    const textInput = myInput.value;
+    const userInput = document.getElementById('username')
+    username.textContent = textInput;
+
+})
+
+//get input 
+//get value from input
+//get div
+//set div content to value
